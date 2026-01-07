9d1346e3aa11dcab9c48275728f61d5f3348b2b3
XpertWebApp <appweb.live@gmail.com>
2026-01-07 18:40:34 +0530

    fixes

--- src/components/frontend/properties/PriceRange.jsx
+++ src/components/frontend/properties/PriceRange.jsx
@@ -1,3 +1,4 @@
+import DownArrowIcon from '@/svg/downArrowIcon';
 import { useState, useEffect } from 'react';
 
 function PriceRange({ min, max, onChange, onChangeMin, onChangeMax, minRange = 0, maxRange = 10000 }) {
@@ -101,7 +102,7 @@ function PriceRange({ min, max, onChange, onChangeMin, onChangeMax, minRange = 0
       <div className="flex items-center justify-between mb-3">
         <h3 className="text-base font-semibold text-text-primary flex items-center gap-2 justify-between w-full">
           Select Price Range
-          <DownArrowIcon />
+          <DownArrowIcon/>
         </h3>
       </div>
       <div className="space-y-4">
